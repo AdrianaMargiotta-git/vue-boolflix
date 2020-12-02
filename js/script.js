598326c58813607dc0b1501776335da69e3407c2
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-12-02 17:45:57 +0100

    milestone 2 ok

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
-
-
 var app = new Vue({
     el: '#app',
     data: {
         cerca: '',
-        listaFilm: []
+        listaFilm: [],
+        listaSerieTv: []
     },
     methods:{
         filtroCerca() {
             // inizia la ricerca inserendo nell'input minimo 2 caratteri
             if (this.cerca.length >= 2){
                 this.films();
+                this.serieTV()
             }
             //cancello l'input
             this.cerca = ''
         },
         // ricerco per film
         films(){
-            axios.get("https://api.themoviedb.org/3/search/movie", {
+            axios.get("https://api.themoviedb.org/3/search/movie", 
+            {
                 params: {
                     api_key: '9dd3a952e16dad35348134e9ee1d2715',
                     lenguage: 'it-IT',
-                    query: this.cerca
+                    query: this.cerca,
+                    answer: 50
                 }
             })
             .then(risposta => {
                 this.listaFilm = risposta.data.results;
             })
-        }
+        },
+        // ricerco per serie tv
+        serieTV(){
+            axios.get("https://api.themoviedb.org/3/search/tv",
+            {
+                params: {
+                    api_key: '9dd3a952e16dad35348134e9ee1d2715',
+                    lenguage: 'it-IT',
+                    query: this.cerca,
+                    answer: 50
+                }
+            })
+            .then(risposta => {
+                this.listaSerieTv = risposta.data.results;
+            })
+        },
     }
 })
